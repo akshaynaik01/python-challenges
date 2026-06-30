0288d7c6450211b4d43d4dd04d73de39bc2b3d59
Akshay Naik <akshaynk292006@gmail.com>
2026-06-30 22:07:42 +0530

    Add personal introduction program in Python

---
+++ 01-personal-introduction.py
@@ -0,0 +1,24 @@
+print("=" * 45)
+print("      PERSONAL INTRODUCTION PROGRAM")
+print("=" * 45)
+
+print("👋 Welcome to my first Python project!")
+print()
+
+print("Name        : Akshay Naik")
+print("Age         : 20")
+print("College     : Dayananda Sagar University")
+print("Course      : B.Tech Computer Science")
+print("Branch      : CSE")
+print("Language    : Python")
+print("Country     : India")
+
+print()
+print("-" * 45)
+
+print("Quote:")
+print('"The expert in anything was once a beginner."')
+
+print("-" * 45)
+print("🚀 Keep Learning | Keep Coding | Never Give Up!")
+print("=" * 45)
